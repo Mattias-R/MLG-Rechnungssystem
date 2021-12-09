1f89ad500d7c6dc266de6dc89d8e97e240a1473a
Lukas <lukas.jaiczay@fh-campuswien.ac.at>
2021-12-09 12:45:07 +0100

    first commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-//test test 2
+//test test 2 test 3
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
